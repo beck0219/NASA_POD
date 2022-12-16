ff1059ce14e111c5a23bf7f2d8416ae590b837b8
mickbeck <mike.beckett7@gmail.com>
2022-12-16 00:24:31 -0600

    Added image update functionality based on when a user has selected a date.

--- app/src/main/java/com/example/nasa_pod/MainActivity.java
+++ app/src/main/java/com/example/nasa_pod/MainActivity.java
@@ -5,6 +5,9 @@ package com.example.nasa_pod;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +15,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,10 +32,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    // POTD object class
     class POTD {
         public String potdTitle;
         public String potdDescription;
@@ -43,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
             this.potdDescription = potdDescription;
         }
     }
+    // POTD adapter class
     class POTDAdapter extends ArrayAdapter<POTD> {
         public POTDAdapter(Context context, ArrayList<POTD> potds) {
             super(context, 0, potds);
@@ -69,13 +80,24 @@ public class MainActivity extends AppCompatActivity {
             return convertView;
         }
     }
+
+    // Get todays date
+    public String getTodaysDate() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+    String TodaysDate = getTodaysDate();
+    String SelectedDateFromUser = TodaysDate;
+
+
     // Getting the JSON data from the API URL
     public class GetJSONDataTask extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... voids) {
             String jsonData = "";
             try {
-                URL url = new URL("https://api.nasa.gov/planetary/apod?api_key=Ykxp8r2fCKlFjcWg54dxy4U3zaWJQ96EuB87Sn6C");
+                URL url = new URL("https://api.nasa.gov/planetary/apod?date="+SelectedDateFromUser+"&api_key=Ykxp8r2fCKlFjcWg54dxy4U3zaWJQ96EuB87Sn6C");
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 connection.connect();
@@ -91,35 +113,33 @@ public class MainActivity extends AppCompatActivity {
             return jsonData;
         }
     }
-    public String getTheImageURL(){
-        String imageurl;
-        GetJSONDataTask task = new GetJSONDataTask();
-        String jsonData = null;
-        try {
-            jsonData = task.execute().get();
-            try {
-                JSONObject obj = new JSONObject(jsonData);
-                imageurl = obj.getString("url");
-                return imageurl;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return null;
+    // Calendar Picker function
+    public void setSelectedDateFromUser(String selectedDateFromUser) {
+        SelectedDateFromUser = selectedDateFromUser;
+    }
+    public String getSelectedDateFromUser() {
+        return SelectedDateFromUser;
+    }
+
+    public void showDatePickerDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        String SelectedDateFromUser = year + "-" + (month + 1) + "-" + dayOfMonth;
+        System.out.println("selectedDate: " + SelectedDateFromUser+"-----------------------------------------------------------------------------------------");
+        setSelectedDateFromUser(SelectedDateFromUser);
+
+
+
+
         ArrayList<POTD> new_image_of_the_day_list = new ArrayList<POTD>();
         POTDAdapter adapter = new POTDAdapter(this, new_image_of_the_day_list);
         ListView listView = findViewById(R.id.image_of_the_day_list);
         listView.setAdapter(adapter);
-        // Getting the JSON data
+
+        // Getting the JSON data and putting it where it needs to be
         GetJSONDataTask task = new GetJSONDataTask();
         String jsonData = null;
         try {
@@ -141,8 +161,84 @@ public class MainActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+    }
+
+
+
+    // Function grabs the image url from the json data, can be deleted later if not used.
+    public String getTheImageURL(){
+        String imageurl;
+        GetJSONDataTask task = new GetJSONDataTask();
+        String jsonData = null;
+        try {
+            jsonData = task.execute().get();
+            try {
+                JSONObject obj = new JSONObject(jsonData);
+                imageurl = obj.getString("url");
+                return imageurl;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // Main section --------------------------------------------------------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ArrayList<POTD> new_image_of_the_day_list = new ArrayList<POTD>();
+//        POTDAdapter adapter = new POTDAdapter(this, new_image_of_the_day_list);
+//        ListView listView = findViewById(R.id.image_of_the_day_list);
+//        listView.setAdapter(adapter);
+
+
+//        // Getting the JSON data and putting it where it needs to be
+//        GetJSONDataTask task = new GetJSONDataTask();
+//        String jsonData = null;
+//        try {
+//            jsonData = task.execute().get();
+//            String title, date, imageurl, description;
+//            try {
+//                JSONObject imageData = new JSONObject(jsonData);
+//                title = imageData.getString("title");
+//                date = imageData.getString("date");
+//                imageurl = imageData.getString("url");
+//                description = imageData.getString("explanation");
+//                POTD potd = new POTD(title, date,imageurl,description);
+//                adapter.add(potd);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        // Printing the ImageURL - if we need it - currently we don't ------------------------------
         String values = getTheImageURL();
         System.out.println(values);
+        // Print stuff here. -----------------------------------------------------------------------
+
+
+        Button button = findViewById(R.id.bPickDate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+
+            }
+        });
+
+//        System.out.println("SelectedDateFromUser: " + SelectedDateFromUser);
+        System.out.println(getSelectedDateFromUser()+ "---------------------------------------------------------------------------------------------------------------------");
+
 
 
     }
